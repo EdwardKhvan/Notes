4de65befe667eb6f2c3b616d63451db50fe25a9d
EdwardKhvan <desertod1288@yandex.ru>
2025-09-16 19:10:12 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Хван Эдуард on 16.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
